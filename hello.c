b4074c0bbd4412536e51f6510e919fe812110bac
andreeich <andreei.chervoniak@gmail.com>
2026-08-30 15:23:10 +0200

    feat: using arrow operator

--- hello.c
+++ hello.c
@@ -6,8 +6,12 @@ struct car {
   int speed;
 };
 
+void set_speed(struct car *c, int speed) { c->speed = speed; }
+
 int main(void) {
   struct car saturn = {.name = "Honda Civik", .price = 123.123};
+
+  set_speed(&saturn, 120);
 
   printf("Name: \t%s\n", saturn.name);
   printf("Price: \t%f\n", saturn.price);
