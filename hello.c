b8ecf4e8f78b15fb62ec05532259586bf1804c31
andreeich <andreei.chervoniak@gmail.com>
2026-08-30 12:19:44 +0200

    feat: dereference operator

--- hello.c
+++ hello.c
@@ -1,7 +1,15 @@
 #include <_printf.h>
 
 int main(void) {
-  int i = 10;
+  int i;
+  int *p;
+
+  p = &i;
+
+  i = 10;
+  *p = 20;
+
   printf("The value of i is %d\n", i);
-  printf("And its address is %p\n", (void *)&i);
+  printf("The value of p is %d\n", *p);
+  printf("And its address is %p\n", (void *)p);
 }
