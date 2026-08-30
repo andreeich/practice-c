dad5f38fa370055aa7f481decd52f0fda59ea05f
andreeich <andreei.chervoniak@gmail.com>
2026-08-30 14:50:35 +0200

    feat: finally string

--- hello.c
+++ hello.c
@@ -1,18 +1,6 @@
 #include <_printf.h>
 
-void print_2d_array(int a[2][3]);
-void print_2d_array(int a[][3]);
-void print_2d_array(int (*a)[3]) {
-  for (int i = 0; i < 2; i++) {
-    for (int j = 0; j < 3; j++) {
-      printf("%d\t", a[i][j]);
-    }
-    printf("\n");
-  }
-}
-
 int main(void) {
-  int a[2][3] = {{1, 2, 3}, {4, 5, 6}};
-
-  print_2d_array(a);
+  char *s = "Hello world!";
+  printf("%s", s);
 }
