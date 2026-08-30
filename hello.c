0c54557f1249548292549178abc9f7ee4dae6e5b
andreeich <andreei.chervoniak@gmail.com>
2026-08-30 15:13:30 +0200

    feat: using structures

--- hello.c
+++ hello.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
-#include <string.h>
+
+struct car {
+  char *name;
+  float price;
+  int speed;
+};
 
 int main(void) {
-  char s[] = "Hello world!";
-  char t[100];
+  struct car saturn;
+  saturn.name = "Honda Civik";
+  saturn.price = 123.123;
+  saturn.speed = 120;
 
-  strcpy(t, s);
-  t[0] = 'b';
-
-  printf("%s\n", s);
-  printf("%s\n", t);
+  printf("Name: \t%s\n", saturn.name);
+  printf("Price: \t%f\n", saturn.price);
+  printf("Speed: \t%d\n", saturn.speed);
 }
