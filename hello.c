c3b12ad4c4ddc7e21e1c0484790608b6e9d8d7f5
andreeich <andreei.chervoniak@gmail.com>
2026-08-30 14:04:43 +0200

    feat: create and initialise 3x3 identity matrix

--- hello.c
+++ hello.c
@@ -1,3 +1,12 @@
 #include <_printf.h>
 
-int main(void) { printf("%zu", sizeof(double[4])); }
+int main(void) {
+  int arr[3][3] = {[0][0] = 1, [1][1] = 1, [2][2] = 1};
+
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      printf("%d\t", arr[i][j]);
+    }
+    printf("\n");
+  }
+}
