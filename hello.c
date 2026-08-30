c6c6fcefc12f2dd8ed6691c677ce64058c2b2b61
andreeich <andreei.chervoniak@gmail.com>
2026-08-30 13:40:40 +0200

    feat: sizeof pointer

--- hello.c
+++ hello.c
@@ -1,15 +1,8 @@
 #include <_printf.h>
 
-void increment(int *p) { *p = *p + 1; }
-
 int main(void) {
-  int i = 10;
-  int *p = &i;
-
-  printf("i is %d\n", i);
-  printf("i is also %d\n", *p);
-
-  increment(p);
-
-  printf("i is %d\n", i);
+  int i;
+  char j;
+  printf("%zu\n", sizeof &i);
+  printf("%zu\n", sizeof &j);
 }
