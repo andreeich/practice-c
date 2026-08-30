dfe37ab3b50127efdaabac1809ba4d461e024ea4
andreeich <andreei.chervoniak@gmail.com>
2026-08-30 13:54:55 +0200

    feat: sizeof an array with fixed length

--- hello.c
+++ hello.c
@@ -1,13 +1,3 @@
 #include <_printf.h>
-#include <stddef.h>
 
-size_t number_of_elements_in_the_array(float f[4]) {
-  return sizeof f / sizeof(float);
-}
-
-int main(void) {
-  float f[4];
-
-  printf("%zu", sizeof f / sizeof(float));
-  printf("%zu", number_of_elements_in_the_array(f));
-}
+int main(void) { printf("%zu", sizeof(double[4])); }
