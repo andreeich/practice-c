7f234f134b4e524f6787142fd8977604cec22bd5
andreeich <andreei.chervoniak@gmail.com>
2026-08-30 14:51:32 +0200

    feat: string with array notation and charater specifier

--- hello.c
+++ hello.c
@@ -1,6 +1,8 @@
 #include <_printf.h>
 
 int main(void) {
-  char *s = "Hello world!";
-  printf("%s", s);
+  char s[] = "Hello world!";
+  for (int i = 0; i < 13; i++) {
+    printf("%c", s[i]);
+  }
 }
