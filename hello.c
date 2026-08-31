a8416e1896d18372225ce3d8f56012318c3bfcf5
andreeich <andreei.chervoniak@gmail.com>
2026-08-31 17:18:12 +0200

    feat: read file stream by lines

--- hello.c
+++ hello.c
@@ -1,13 +1,15 @@
+#include <_stdio.h>
 #include <stdio.h>
 
 int main(void) {
   FILE *fp;
-  int c;
+  char s[1024];
+  int linecount = 0;
 
   fp = fopen("hello.txt", "r");
 
-  while ((c = fgetc(fp)) != EOF) {
-    printf("%c", c);
+  while ((fgets(s, sizeof s, fp)) != NULL) {
+    printf("%d: %s", ++linecount, s);
   }
 
   fclose(fp);
