5d52a52b20f384e4b24a4c843dc87f625ff09482
andreeich <andreei.chervoniak@gmail.com>
2026-08-31 20:59:36 +0200

    feat: scan formatted input

--- hello.c
+++ hello.c
@@ -1,15 +1,15 @@
-#include <_stdio.h>
 #include <stdio.h>
 
 int main(void) {
   FILE *fp;
-  char s[1024];
-  int linecount = 0;
+  char name[1024];
+  float length;
+  int mass;
 
   fp = fopen("hello.txt", "r");
 
-  while ((fgets(s, sizeof s, fp)) != NULL) {
-    printf("%d: %s", ++linecount, s);
+  while ((fscanf(fp, "%s %f %d", name, &length, &mass)) != EOF) {
+    printf("%s whale, %d tones, %.1f meters\n", name, mass, length);
   }
 
   fclose(fp);
