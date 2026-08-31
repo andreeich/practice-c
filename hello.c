6623a4d66ba3626a4bf39fae0ff63a652de61900
andreeich <andreei.chervoniak@gmail.com>
2026-08-31 17:05:17 +0200

    feat: file stream char read

--- hello.c
+++ hello.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 
-struct car {
-  char *name;
-  float price;
-  int speed;
-};
-
-void set_speed(struct car *c, int speed) { c->speed = speed; }
-
 int main(void) {
-  struct car saturn = {.name = "Honda Civik", .price = 123.123};
+  FILE *fp;
+  int c;
 
-  set_speed(&saturn, 120);
+  fp = fopen("hello.txt", "r");
 
-  printf("Name: \t%s\n", saturn.name);
-  printf("Price: \t%f\n", saturn.price);
-  printf("Speed: \t%d\n", saturn.speed);
+  while ((c = fgetc(fp)) != EOF) {
+    printf("%c", c);
+  }
+
+  fclose(fp);
 }
