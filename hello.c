80dac172fb7a11d55c1e0b51ac37a14fb859f91c
andreeich <andreei.chervoniak@gmail.com>
2026-09-02 22:27:40 +0200

    feat: basic pointer arithmetic

--- hello.c
+++ hello.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-typedef struct {
-  int x, y;
-} point;
-
 int main(void) {
-  point p = {.x = 1, .y = 2};
+  int a[5] = {1, 2, 3, 4, 5};
+  int *p = &a[0];
 
-  printf("%d %d\n", p.x, p.y);
+  printf("%d\n", *p);
+  printf("%d\n", *(p + 0));
+  printf("%d\n", *(p + 1));
 }
