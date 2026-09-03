3108ac00e634857f7bfc82f5874d3b38a0ae7bdd
andreeich <andreei.chervoniak@gmail.com>
2026-09-03 21:45:53 +0200

    feat: implement struct sorting function for qsort

--- hello.c
+++ hello.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
-#include <string.h>
-
-void *new_memcpy(void *dest, void *src, int byte_count) {
-  char *d = dest, *s = src;
-  while (byte_count--) {
-    *d++ = *s++;
-  }
-  return dest;
-}
+#include <stdlib.h>
 
 struct animal {
   char *name;
+  int leg_count;
 };
 
+int compar(const void *elem1, const void *elem2) {
+  const struct animal *animal1 = elem1, *animal2 = elem2;
+
+  if (animal1->leg_count > animal2->leg_count) {
+    return 1;
+  }
+  if (animal1->leg_count < animal2->leg_count) {
+    return -1;
+  }
+  return 0;
+}
+
 int main(void) {
-  struct animal cat = {.name = "Lucy"};
-  struct animal dog;
+  struct animal a[4] = {
+      {.name = "Dog", .leg_count = 4},
+      {.name = "Monkey", .leg_count = 2},
+      {.name = "Anthelope", .leg_count = 4},
+      {.name = "Snake", .leg_count = 0},
+  };
 
-  new_memcpy(&dog, &cat, sizeof cat);
+  qsort(a, 4, sizeof(struct animal), compar);
 
-  printf("%s", dog.name);
+  for (int i = 0; i < 4; i++) {
+    printf("%d: %s\n", a[i].leg_count, a[i].name);
+  }
 }
