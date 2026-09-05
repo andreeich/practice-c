b591904eca05744a36b1aee7a69483528bf2e4ea
andreeich <andreei.chervoniak@gmail.com>
2026-09-05 16:00:21 +0200

    feat: malloc and memset

--- hello.c
+++ hello.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void) {
   int *p = malloc(sizeof(*p) * 10);
 
   if (p == NULL) {
     printf("Error allocating 10 ints!");
+  }
+
+  memset(p, 0, sizeof(*p) * 10);
+
+  for (int i = 0; i < 10; i++) {
+    printf("%d\n", p[i]);
   }
 
   for (int i = 0; i < 10; i++) {
