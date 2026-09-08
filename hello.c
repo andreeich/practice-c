6ae1673555c3e2ad4aa4f3f4f24186fb98f8c9cf
andreeich <andreei.chervoniak@gmail.com>
2026-09-08 16:36:55 +0200

    feat: comparing decimal digits of float and double

--- hello.c
+++ hello.c
@@ -1,29 +1,10 @@
+#include <float.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void) {
-  float *p = malloc(20 * sizeof *p);
-
-  for (int i = 0; i < 20; i++) {
-    *(p + i) = i / 20.0;
-  }
-
-  float *new_p = realloc(p, 40 * sizeof *p);
-
-  if (new_p == NULL) {
-    printf("Error while realloc");
-    return 1;
-  }
-
-  p = new_p;
-
-  for (int i = 20; i < 40; i++) {
-    *(p + i) = 1 + (i - 20) / 20.0;
-  }
-
-  for (int i = 0; i < 40; i++) {
-    printf("%f\n", p[i]);
-  }
-
-  free(p);
+  printf("%d %d %d\n", FLT_DIG, DBL_DIG, LDBL_DIG);
+  double a = 1.23456789123456789;
+  float b = a;
+  printf("%.16f\n", a);
+  printf("%.16f\n", b);
 }
