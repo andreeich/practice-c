260bbca682be31a2968ad9b2cfaf8ffed91049c3
andreeich <andreei.chervoniak@gmail.com>
2026-09-08 12:02:34 +0200

    feat: filling extended array after realloc

--- hello.c
+++ hello.c
@@ -2,16 +2,27 @@
 #include <stdlib.h>
 
 int main(void) {
-  int *p = calloc(10, sizeof(*p));
-
-  if (p == NULL) {
-    printf("Error allocating 10 ints!");
-  }
-
-  p = realloc(p, sizeof(*p) * 20);
+  float *p = malloc(20 * sizeof *p);
 
   for (int i = 0; i < 20; i++) {
-    printf("%d\n", p[i]);
+    *(p + i) = i / 20.0;
+  }
+
+  float *new_p = realloc(p, 40 * sizeof *p);
+
+  if (new_p == NULL) {
+    printf("Error while realloc");
+    return 1;
+  }
+
+  p = new_p;
+
+  for (int i = 20; i < 40; i++) {
+    *(p + i) = 1 + (i - 20) / 20.0;
+  }
+
+  for (int i = 0; i < 40; i++) {
+    printf("%f\n", p[i]);
   }
 
   free(p);
